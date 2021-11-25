1579894a6c0da79221a941e927189e839deb0547
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-25 21:18:52 +0100

    Add fonts

--- App.js
+++ App.js
@@ -1,8 +1,30 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font'
+import Apploading from 'expo-app-loading'
+
+const fetchFont = () => {
+  return Font.loadAsync({
+    "regular" : require('./assets/fonts/Roboto-Bold.ttf'),
+    "bold" : require('./assets/fonts/Roboto-Regular.ttf')
+  })
+}
 
 export default function App() {
+  const [isLoaded, setisLoaded] = useState(false)
+
+  if (!isLoaded) {
+    return (
+      <AppLoading
+      startAsync={fetchFont}
+      onFinish={() => setisLoaded(true)}
+      onError={console.warn}
+    />
+  ); 
+
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
