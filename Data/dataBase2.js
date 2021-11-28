b49e56bc9fb510f65f2b96384f799030bf4f2db3
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-11-28 15:18:25 +0100

    Setting navigation + getParam + navigationOptions

---
+++ Data/dataBase2.js
@@ -0,0 +1,17 @@
+import categorie from '../models/categorieModel'
+export const categoriesData = [
+    new categorie(1,'boissons', 'red'),
+    new categorie(2,'chips', 'red'),
+    new categorie(3,'chocolats', 'red'),
+    new categorie(4,'bonbons', 'red'),
+    new categorie(5,'biscuits', 'red'),
+
+
+]
+
+// Une autre méthode pour créer une dataBase
+
+export const articles = [
+    {}
+
+]
