98261c7b99f6ea68263096bb9bd0a04b525619b9
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-12-04 12:45:00 +0100

    Render all categories in FlatList

--- Screens/Categories.jsx
+++ Screens/Categories.jsx
@@ -1,14 +1,64 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, Text, Button, FlatList } from 'react-native'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import BoxShadow from '../Component/BoxShadow'
+import {CATEGORIES} from '../Data/dataBase2'
+function Categories(props) {
+    // console.log(CATEGORIES)
+const renderItem = (itemData) => {
+    // console.log(itemData)
 
-export default function Categories() {
     return (
-        <View>
-            <Text> Categories </Text>
+            <TouchableOpacity
+            activeOpacity={0.7}
+            style={css.box}
+            onPress={() => props.navigation.navigate('CategorieScreen', {
+                catId : itemData.item.id,
+                catName : itemData.item.name,
+            })}
+            >
+            <BoxShadow otherStyle={css.shadow}>
+           <Text> {itemData.item.name} </Text>
+            </BoxShadow>
+            </TouchableOpacity>
+    )
+}
+    return (
+        <View style={css.container}>
+        <FlatList
+        data={CATEGORIES}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+        numColumns={2}
+      />
         </View>
     )
 }
 
+Categories.navigationOptions = () => {
+    return {
+        title: 'Home',
+      };
+}
+
 const css=StyleSheet.create({
+    container : {
+        margin: 10,
+        flex : 1,
+        justifyContent : 'center',
+        alignItems : 'center',
+    },
+    box : {
+        height: 150,
+        width: '100%',
+        margin: 2,
+        flex: 1,
+    },
+    shadow : {
+        width: '100%',
+        height: '100%',
+    }
 
 })
+
+export default Categories
