eab546ce0160c50ec02c28553ba3a4639ec37dd2
RaedGhodhbane <ghodhbane.raed@gmail.com>
2021-12-04 12:47:29 +0100

    Render articles with the same catId in dynamic Categorie screen

--- Screens/Categorie.jsx
+++ Screens/Categorie.jsx
@@ -1,14 +1,47 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, View, FlatList, Image, Text, TouchableOpacity} from 'react-native'
+import Card from '../Component/Card'
+import {ARTICLES} from '../Data/dataBase2'
 
-export default function Categorie() {
+
+export default function Categorie(props) {
+    // console.log(props.navigation.getParam('catId'))
+const catId = props.navigation.getParam('catId')
+const currentArticles = ARTICLES.filter(articles => articles.catId === catId)
+// console.log(currentArticles)
+const selectItem = (itemData) => {
+    props.navigation.navigate('DetailScreen', {
+        itemId : itemData.item.id
+    })
+}
+const renderItem = (itemData) => {
+// console.log(itemData)
+
+    return (
+            <Card itemData={itemData} selectItem = {selectItem} />
+    )
+}
     return (
         <View>
-            <Text> Categorie </Text>
+        <FlatList
+        style={css.listItem}
+        data={currentArticles}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
+
         </View>
     )
 }
 
+Categorie.navigationOptions = (props) => {
+    return {
+        title : props.navigation.getParam('catName'),
+    };
+        
+}
 const css=StyleSheet.create({
-
+    listItem : {
+        paddingHorizontal : 15,
+    }
 })
